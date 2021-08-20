42ff238aba0fcbf4ca5c6740cb2ddf5c1184aa7d
maxhardy2013 <46877175+maxhardy2013@users.noreply.github.com>
2021-08-20 02:46:55 -0400

    front pages changed

---
+++ public/js/sellersHome.js
@@ -0,0 +1,17 @@
+function createNewOrder(){
+    //redicrect from sellersHome page to createOrder page
+};
+
+
+function viewHistoryOrders(){
+    //redicrect from sellersHome page to viewOrders page
+
+
+};
+
+
+function viewProductions(){
+    //redicrect from sellersHome page to viewProductions page
+
+
+};
