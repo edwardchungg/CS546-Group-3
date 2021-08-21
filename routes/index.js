917cd3093d114442c4e03e1ca6f82c54009d2bc2
DxPtl01 <87237633+DxPtl01@users.noreply.github.com>
2021-08-21 15:59:18 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router  = express.Router();
-//login page
+//welcome page
 router.get('/', (req,res)=>{
     res.render('welcome');
 })
+// login page
+router.get('/auth/login', (req,res)=>{
+    res.render()
+})
 //register page
-router.get('/signup', (req,res)=>{
+router.get('/auth/signup', (req,res)=>{
     res.render('register');
 })
 
