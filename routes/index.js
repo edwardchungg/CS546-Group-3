43c3dc249525a40ebbde8ff6a2f07c579925fc48
DxPtl01 <87237633+DxPtl01@users.noreply.github.com>
2021-08-21 20:13:31 -0400

    index

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,29 @@
-//cindesx.js
+
+const userRoutes = require("./users");
+const inventoryRoutes = require("./inventory");
+const orderRoutes = require("./order");
+
+
+const constructorMethod = (app) => {
+  app.use("/user", userRoutes);
+  app.use("/inventory", inventoryRoutes);
+  app.use("/order", orderRoutes);
+  
+  app.get("/logout", async (req, res) => {
+    // trigger /logout router to logout
+    res.clearCookie("AuthCookie");
+    res.clearCookie("Build Session");
+    req.session.destroy();
+    res.status(200).redirect("/user");
+  });
+
+  app.use("/", async (req, res) => {
+    res.redirect("/user");
+  });
+
+  app.use("*", (req, res) => {
+    res.sendStatus(404);
+  });
+};
+
+module.exports = constructorMethod;
