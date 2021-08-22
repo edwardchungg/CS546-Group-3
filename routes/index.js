076ad19fa258f7d71892def5c0fdf873f413f1f0
DxPtl01 <87237633+DxPtl01@users.noreply.github.com>
2021-08-22 11:44:24 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,32 @@
 
-const userRoutes = require("./users");
-const inventoryRoutes = require("./inventory");
-const orderRoutes = require("./order");
-
+const registerRoute = require("./register");
+const loginRoute = require("./login");
+const privateRoute = require("./private");
+const logoutRoute = require("./logout");
+const userDeleteRoute = require("./userDeleteRoute");
+const userUpdateRoute = require("./userUpdateRoute");
+const inventoryRoutes = require("./inventoryRoutes");
+const orderRoutes = require("./orderRoutes");
 
 const constructorMethod = (app) => {
-  app.use("/user", userRoutes);
+  app.get("/", function (req, res) {
+    res.render("pages/index");
+  });
+  app.use("/register", registerRoute);
+  app.use("/login", loginRoute);
+  app.use("/logout", logoutRoute);
+  app.use("/private", privateRoute);
+  app.use("/userDelete", userDeleteRoute);
+  app.use("/userUpdate", userUpdateRoute);
   app.use("/inventory", inventoryRoutes);
   app.use("/order", orderRoutes);
-  
-//   app.get("/logout", async (req, res) => {
-//     // trigger /logout router to logout
-//     res.clearCookie("AuthCookie");
-//     res.clearCookie("Build Session");
-//     req.session.destroy();
-//     res.status(200).redirect("/user");
-//   });
-
-  app.use("/", async (req, res) => {
-    res.redirect("/user");
-  });
-
-  app.use("*", (req, res) => {
-    res.sendStatus(404);
+  app.get("/getSession", async (req, res) => {
+    if (req.session.user) {
+      res.json({ result: true, name: req.session.user.email });
+      return;
+    }
+    res.json({ result: false });
+    return;
   });
 };
 
