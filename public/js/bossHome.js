11cbf37c9d51ec1543b8de6e2fe55fbc2087deee
maxhardy2013 <46877175+maxhardy2013@users.noreply.github.com>
2021-08-22 22:10:11 -0400

    front pages update

--- public/js/bossHome.js
+++ public/js/bossHome.js
@@ -6,10 +6,3 @@ function gotoInventory(){
     //redicrect from bossHome page to inventoryHome page
 };
 
-function gotoDelivery(){
-    //redicrect from bossHome page to deliveryHome page
-};
-
-function gotoFinancial(){
-    //redicrect from bossHome page to financialHome page
-};
